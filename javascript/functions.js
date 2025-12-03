930303f824a8d1efc5123dad37ba121cb27ad659
Dee man <damilaretolulope1@gmail.com>
2025-12-03 21:06:28 +0100

    javascript functions

---
+++ javascript/functions.js
@@ -0,0 +1,17 @@
+console.log("This is from the function's script!!");
+
+// Functions
+// declare a function
+function greetUser() {
+    console.log("Hello, and welcome to my webpage!");
+}
+
+function showStudentName(name, course) {
+    console.log(`My name is ${name} and I am a student of Axia Africa learning ${course} course.`)
+}
+
+// call a function
+greetUser();
+showStudentName("Claire", "Frontend Development");
+showStudentName("Abolaji", "Backend Development");
+showStudentName();
